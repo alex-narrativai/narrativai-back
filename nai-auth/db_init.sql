d02129754cea58ba5231d2aaadb439e2c73f0abd
Alex Springer <128489498+alex-narrativai@users.noreply.github.com>
2023-08-26 06:27:40 +0000

    Reset

--- nai-auth/db_init.sql
+++
@@ -1,9 +0,0 @@
--- Create User table
-CREATE TABLE IF NOT EXISTS User (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT UNIQUE NOT NULL,
-    password TEXT NOT NULL
-);
-
--- Add index on email for faster lookup
-CREATE UNIQUE INDEX IF NOT EXISTS idx_user_email ON User(email);
